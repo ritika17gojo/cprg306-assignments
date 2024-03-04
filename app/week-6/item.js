e19a4f744e58b66a95761d880345eace649fb365
ritika17gojo <ritika.malik@edu.sait.ca>
2024-03-04 15:59:33 -0700

    git add.
    git commit-m "Added files to folder"

---
+++ app/week-6/item.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Item = ({ name,quantity, category}) => {
+    return (
+        <li className = "bg-white rounded-lg overflow-hidden shadow-lg transform transition duration-500 hover:scale-105">
+            <div className="p-5">
+                <p className="text-lg font-semibold tracking-wide text-gray-600">{name}</p>
+                <p className="text-indigo-500">{category}</p>
+                <p className="text-gray-600">Quantity: {quantity}</p>
+            </div>
+        </li>
+    );
+};
+
+export default Item;
