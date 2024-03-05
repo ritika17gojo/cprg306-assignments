fe3dcc5ec45fd93e74056d242406b6398efefc86
ritika17gojo <ritika.malik@edu.sait.ca>
2024-03-05 13:03:56 -0700

    git add.
    git commit -m "Added files to folder"

--- app/week-6/item-list.js
+++ app/week-6/item-list.js
@@ -60,7 +60,7 @@ category = {item.category} />
         <div className="w-full max-w-4xl mx-auto mt-8">
             <div className="flex justify-center mb-4 space-x-3">
                 <button onClick={() => setSortBy("name")}
-                className={'relative inline-flex items-center justify-center px-6 py-3 font-semibold rounded-full overflow-hidden group ${sortBy === "name" ? "bg-gradient-to-br from-purple-500 to-pink-500" : "bg-gradient-to-br from-gray-400 to-blue-400"} text-white shadow-lg transition-all duration-300 ease-out hover:from-green-500 haver:to-blue-500 hover: scale-110'} 
+                className={'relative inline-flex items-center justify-center px-6 py-3 font-semibold rounded-full overflow-hidden group ${sortBy === "name" ? "bg-gradient-to-br from-gray-500 to-gray-500" : "bg-gradient-to-br from-gray-400 to-blue-400"} text-white shadow-lg transition-all duration-300 ease-out hover:from-gray-500 hover:to-blue-500 hover: scale-110'} 
                 >
                     <svg xmlns="http://www.w3.org/2000/svg" className={'h-6 w-6 mr-2 transition-transform duration-300 ${sortBy ==="category" ? "rotate-0" : "rotate-180"}'} fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
@@ -69,7 +69,7 @@ category = {item.category} />
                         </span> 
                     </button>
                     <button onClick={() => setSortBy("grouped")}
-                    className={'relative inline-flex items-center justify-center px-6 py-3 font-semibold rounded-full overflow-hidden group ${sortBy === "grouped" ? "bg-gradient-to-br from-purple-500 to-pink-500" : "bg-gradient-to-br from gray-400 to-blue-400"} text-white shadow-lg transition-all duration-300 ease-out hover:from-green-500 hover:to-blue-500 hover:scale-110'}
+                    className={'relative inline-flex items-center justify-center px-6 py-3 font-semibold rounded-full overflow-hidden group ${sortBy === "grouped" ? "bg-gradient-to-br from-gray-500 to-blue-500" : "bg-gradient-to-br from gray-400 to-blue-400"} text-white shadow-lg transition-all duration-300 ease-out hover:from-gray-500 hover:to-blue-500 hover:scale-110'}
                     >
                         <svg xmlns="http://www.w3.org/2000/svg" className={'h-6 w-6 mr-2 transition-transform duration-300 ${sortBy=== "grouped" ? "rotate-0" : "rotate-180"}'} fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M29 9l-7 7-7-7" />
--- app/week-6/item.js
+++ app/week-6/item.js
@@ -5,7 +5,7 @@ const Item = ({ name,quantity, category}) => {
         <li className = "bg-white rounded-lg overflow-hidden shadow-lg transform transition duration-500 hover:scale-105">
             <div className="p-5">
                 <p className="text-lg font-semibold tracking-wide text-gray-600">{name}</p>
-                <p className="text-indigo-500">{category}</p>
+                <p className="text-blue-500">{category}</p>
                 <p className="text-gray-600">Quantity: {quantity}</p>
             </div>
         </li>
